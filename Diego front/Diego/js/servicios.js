0c96d7524450ae851f22c7932cd4445d3cd0af3a
DiegoFlores12 <diegofloresa415@gmail.com>
2025-07-08 21:24:04 -0400

    Delete Diego front/Diego/js/servicios.js

--- Diego front/Diego/js/servicios.js
+++
@@ -1,5 +0,0 @@
-async function obtenerTodos(entidad) {
-  const res = await fetch(`http://localhost:3000/${entidad}`);
-  if (!res.ok) throw new Error(`Error al obtener ${entidad}`);
-  return await res.json();
-}
